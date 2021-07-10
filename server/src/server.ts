e70af0575695a6e05a69a7fc72ba3077e9d00705
Roei Bracha <roei635@gmail.com>
2021-07-10 18:56:36 +0300

    remove the cors for now

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,17 +8,17 @@ const app = express ();
 app.use(express.json());
 app.use(cookieParser())
 
-if (process.env.NODE_ENV === "production" || true) {
-    app.use(csrf({ cookie: true }));
-    // const corsOptions: cors.CorsOptions= {
-    //     origin: process.env.SITE_URL ? process.env.SITE_URL : "http://localhost:3000",
-    //     credentials: true,
-    // }
-    app.all("*", (req: express.Request, res: express.Response, next: express.NextFunction) => {
-        res.cookie("XSRF-TOKEN", req.csrfToken());
-        next();
-    })
-}
+// if (process.env.NODE_ENV === "production" || true) {
+//     app.use(csrf({ cookie: true }));
+//     // const corsOptions: cors.CorsOptions= {
+//     //     origin: process.env.SITE_URL ? process.env.SITE_URL : "http://localhost:3000",
+//     //     credentials: true,
+//     // }
+//     app.all("*", (req: express.Request, res: express.Response, next: express.NextFunction) => {
+//         res.cookie("XSRF-TOKEN", req.csrfToken());
+//         next();
+//     })
+// }
 
 app.get('/',(req :Request , res:Response)=>{
     res.send("hello world")
